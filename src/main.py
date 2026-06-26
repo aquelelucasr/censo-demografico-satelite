50ed60b638715b20ea712a22b384c421afc80ce8
Mess <45535471+Messier1071@users.noreply.github.com>
2026-06-26 01:16:14 -0300

    main calls Ui properly

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
-from app.Controller.Functions import *
-from app.Model.databaseModel import setup_db
-
+from controller.Functions import *
+from model.databaseModel import setup_db
+from view import tela_default
 
 setup_db()
 setup_api_keys()
 
+get_map_image(-28.9486767,-49.4689805,-28.9513217,-49.4638693)
+
+tela_default.start_window_loop()
 
 
 
